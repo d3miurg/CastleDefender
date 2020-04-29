6a5535b92a976246beb8f39ad440f611852e87ea
d3miurg <whu19551@gmail.com>
2020-04-29 22:00:08 +0300

    added movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform cam;
+
+    private Vector3 nowMousePos;
+    private Vector3 lastMousePos;
+
+    void Start()
+    {
+
+        lastMousePos = Input.mousePosition;
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            lastMousePos = Input.mousePosition;
+
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+
+            nowMousePos = Input.mousePosition;
+
+            cam.position += cam.right * (lastMousePos.x - nowMousePos.x) * Time.deltaTime;
+            cam.position += cam.up * (lastMousePos.y - nowMousePos.y) * Time.deltaTime;
+
+            lastMousePos = Input.mousePosition;
+
+        }
+
+    }
+}
